ee3a6b22029a885d9b8a31aea7be92a389407cd7
Carlos Caguana <marranitocarlitos@gmail.com>
2026-06-29 17:38:34 -0400

    Carpeta Orders creada

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 import { FilesModule } from './files/files.module';
 import { AuthModule } from './auth/auth.module';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
   imports: [
@@ -30,6 +31,8 @@ import { AuthModule } from './auth/auth.module';
     FilesModule,
 
     AuthModule,
+
+    OrdersModule,
   ],
 
 })
