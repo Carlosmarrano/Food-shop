68944cda7e24b6f71aa69ba77f01d1c9bb0f6a16
Carlos Caguana <marranitocarlitos@gmail.com>
2026-06-29 19:49:11 -0400

    Logica de Delete  Update y FindById agregados al orderService

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -72,19 +72,53 @@ export class OrdersService {
 
   }
 
-  findAll() {
-    return `This action returns all orders`;
+  async findOrder(id: string) {
+
+    const order = await this.orderRepository.findOne({
+      where: {id},
+      relations: {
+        items: {
+          food: true
+        },
+        user: true
+      }
+    });
+
+    if (!order) {
+      throw new NotFoundException(`The order with ID ${id} was not found`)
+    }
+
+    return order;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} order`;
+  async update(id: string, updateOrderDto: UpdateOrderDto) {
+    
+    const order = await this.orderRepository.preload({
+      id,
+      ...updateOrderDto
+    });
+
+    if (!order){
+      throw new NotFoundException(`The order with ID ${id} does not exist`)
+    }
+
+    try{
+      await this.orderRepository.save(order);
+      
+      return order;
+    } catch(error){
+      throw new InternalServerErrorException("Unexpected error updating order")
+    }
   }
 
-  update(id: number, updateOrderDto: UpdateOrderDto) {
-    return `This action updates a #${id} order`;
-  }
+  async remove(id: string) {
+    
+    const order = await this.findOrder(id);
 
-  remove(id: number) {
-    return `This action removes a #${id} order`;
+    await this.orderRepository.remove(order);
+
+    return { 
+      message: `The order ${id} and its concepts have been successfully deleted`
+    }
   }
 }
