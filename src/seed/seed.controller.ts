f6e4b7024b91836cb50200d06fbe50558179ed2f
Carlos Caguana <marranitocarlitos@gmail.com>
2026-08-30 21:37:41 -0400

    Protegiendo ruta del seed con clave secreta por URL

--- src/seed/seed.controller.ts
+++ src/seed/seed.controller.ts
@@ -1,14 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query, UnauthorizedException } from '@nestjs/common';
 import { SeedService } from './seed.service';
 import { Auth } from 'src/auth/decorators/auth.decorator';
+import { ConfigService } from '@nestjs/config';
 
-@Controller('seed') 
+@Controller('seed')
 export class SeedController {
-  constructor(private readonly seedService: SeedService) {}
+  constructor(
+    private readonly seedService: SeedService,
+    private readonly configService: ConfigService,
+  ) { }
 
   @Get('food')
   @Auth()
-  executeSeedFood(){
+  executeSeedFood(@Query('key') key: string) {
+
+    const seedSecret = this.configService.get("SEED_SECRET") || "mi_clave_Secreta_123";
+
+    if (key !== seedSecret) {
+      throw new UnauthorizedException("clave de seed inválida");
+    }
+
     return this.seedService.runSeedFood();
   }
 }
