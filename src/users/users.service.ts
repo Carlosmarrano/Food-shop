bf16b5c278bab16201a653486476618269962e54
Carlos Caguana <marranitocarlitos@gmail.com>
2026-09-04 21:10:53 -0400

    Injectando la entidad User en usersService & Agregando funcionalidad en el método FindAll

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,19 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { CreateUserDto } from './dto/create-user-dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
+
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+
+  ) { }
+
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
 
-  findAll() {
-    return `This action returns all users`;
-  }
+  async findAll() {
+    return await this.userRepository.find({
+      select: ["id", "fullName", "email", "roles", "isActive"],
+    });
+  };
 
   findOne(id: number) {
-    return `This action returns a #${id} user`;
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
