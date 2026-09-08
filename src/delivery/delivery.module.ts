9660deaf45213b84279a1140be3295fefc0dfb1f
Carlos Caguana <marranitocarlitos@gmail.com>
2026-09-07 23:34:08 -0400

    Importando la entidad de usuario en el módulo de delivery

--- src/delivery/delivery.module.ts
+++ src/delivery/delivery.module.ts
@@ -4,12 +4,13 @@ import { DeliveryController } from './delivery.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Delivery } from './entities/delivery.entity';
 import { Order } from '../orders/entities/order.entity';
+import { User } from 'src/users/entities/user.entity';
 
 @Module({
   controllers: [DeliveryController],
   providers: [DeliveryService],
   imports: [
-    TypeOrmModule.forFeature([Delivery, Order])
+    TypeOrmModule.forFeature([Delivery, Order, User])
   ],
 })
 export class DeliveryModule { }
