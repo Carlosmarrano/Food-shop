683b7c0d2216b082a38f5ad949e2b829e399833b
Carlos Caguana <marranitocarlitos@gmail.com>
2026-09-09 13:48:20 -0400

    Agregando los controladores respectivos a cada método dentro del deliveryController

--- src/delivery/delivery.controller.ts
+++ src/delivery/delivery.controller.ts
@@ -1,34 +1,41 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseUUIDPipe } from '@nestjs/common';
 import { DeliveryService } from './delivery.service';
 import { CreateDeliveryDto } from './dto/create-delivery.dto';
 import { UpdateDeliveryDto } from './dto/update-delivery.dto';
+import { UpdateLocationDto } from './dto/update-location.dto';
+import { AsignOrderDto } from './dto/asing-order.dto';
 
 @Controller('delivery')
 export class DeliveryController {
-  constructor(private readonly deliveryService: DeliveryService) {}
+  constructor(private readonly deliveryService: DeliveryService) { }
 
   @Post()
   create(@Body() createDeliveryDto: CreateDeliveryDto) {
     return this.deliveryService.create(createDeliveryDto);
   }
 
-  @Get()
-  findAll() {
-    return this.deliveryService.findAll();
+  @Post(':id/asing')
+  asingOrder(@Param('id', ParseUUIDPipe) id: string, @Body() asingOrderDto: AsignOrderDto) {
+    return this.deliveryService.asingOrder(id, asingOrderDto);
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.deliveryService.findOne(+id);
+  @Get('/available')
+  getAvailableDrivers() {
+    return this.deliveryService.getAvailableDrivers();
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateDeliveryDto: UpdateDeliveryDto) {
-    return this.deliveryService.update(+id, updateDeliveryDto);
+  @Get(':id/metrics')
+  getDriverMetrics(@Param('id', ParseUUIDPipe) id: string) {
+    return this.deliveryService.getDriverMetrics(id);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.deliveryService.remove(+id);
+  @Patch(':id/status')
+  changeShiftStatus(@Param('id', ParseUUIDPipe) id: string, @Body() updateDeliveryDto: UpdateDeliveryDto) {
+    return this.deliveryService.changeShiftStatus(id, updateDeliveryDto);
+  }
+
+  @Patch(':id/location')
+  updateLocation(@Param('id', ParseUUIDPipe) id: string, @Body() updateLocationDto: UpdateLocationDto) {
+    return this.deliveryService.updateLocation(id, updateLocationDto);
   }
 }
