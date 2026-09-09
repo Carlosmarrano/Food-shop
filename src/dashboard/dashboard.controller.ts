3a4fdcd41f88313dc916475fd22ee3776a2f6211
Carlos Caguana <marranitocarlitos@gmail.com>
2026-09-09 18:06:48 -0400

    Agregando controladores a los respectivos métodos de dashboardService

--- src/dashboard/dashboard.controller.ts
+++ src/dashboard/dashboard.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { DashboardService } from './dashboard.service';
 
 @Controller('dashboard')
-export class DashboardController {}
+export class DashboardController {
+    constructor(private readonly dashboardService: DashboardService) { }
+
+    @Get('metrics')
+    getGlobalMetrics() {
+        return this.dashboardService.getGlobalMetrics();
+    }
+
+    @Get('weekly-sales')
+    getWeeklySales() {
+        return this.dashboardService.getWeeklySales();
+    }
+}
