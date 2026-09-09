922d9bda2353f5ac1525cc5902d9cc0f864e86a9
Carlos Caguana <marranitocarlitos@gmail.com>
2026-09-09 17:43:39 -0400

    Creación del método getGlobalMetrics en el dashboardService

--- src/dashboard/dashboard.service.ts
+++ src/dashboard/dashboard.service.ts
@@ -1,4 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { foodStatus, Order } from 'src/orders/entities/order.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class DashboardService {}
+export class DashboardService {
+
+    constructor(
+        @InjectRepository(Order) private readonly orderRepository: Repository<Order>,
+    ) { }
+
+    async getGlobalMetrics() {
+
+        const totalOrders = await this.orderRepository.count();
+
+        const completedOrders = await this.orderRepository.count({
+            where: { status: foodStatus.delivered },
+        });
+
+        const activeOrders = await this.orderRepository.count({
+            where: { status: foodStatus.pending }
+        })
+
+        const cancelledOrders = await this.orderRepository.count({
+            where: { status: foodStatus.cancelled },
+        });
+
+        return {
+            totalOrders,
+            completedOrders,
+            activeOrders,
+            cancelledOrders,
+        };
+    };
+}
